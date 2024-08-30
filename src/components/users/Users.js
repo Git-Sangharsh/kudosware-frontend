02103c2db5febdc0573605a3cf7fb5db86037411
sangharsh <sangharshdev69@gmail.com>
2024-08-30 05:49:32 +0530

    Final Commmit

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -8,7 +8,9 @@ const Users = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get("https://kudosware-backend-q3ha.onrender.com/users"); // Replace with your backend URL
+        const response = await axios.get(
+          "https://kudosware-backend-q3ha.onrender.com/users"
+        ); // Replace with your backend URL
         setData(response.data);
       } catch (error) {
         setError("Error fetching user data");
@@ -28,13 +30,13 @@ const Users = () => {
           <div className="users-wrapper-box" key={user._id}>
             <h5 className="users-wrapper-title">Name : {user.name} </h5>
             <h5 className="users-wrapper-title">Email : {user.email} </h5>
-            <button className="download-resume">
-              <a
-                href={`https://kudosware-backend-q3ha.onrender.com/users/${user._id}/resume`}
-                download
-              >
-                Download Resume
-              </a>
+            <button
+              className="download-resume"
+              onClick={() => {
+                window.location.href = `https://kudosware-backend-q3ha.onrender.com/users/${user._id}/resume`;
+              }}
+            >
+              Download Resume
             </button>
           </div>
         ))}
